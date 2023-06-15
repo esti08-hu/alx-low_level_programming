a1e5d5ac6c5e5558a9ed051b0375bb70004e1c2b
esti08-hu <estioame@gmail.com>
2023-06-15 15:21:15 -0700

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)n", sizeOf(char));
+	printf("Size of a int: %d byte(s)n", sizeOf(int));
+	printf("Size of a long int: %d byte(s)n", sizeOf(long int));
+	printf("Size of a long long int: %d byte(s)n", sizeOf(long long int));
+	printf("Size of a float: %d byte(s)n", sizeOf(float));
+    return (0);
+}
