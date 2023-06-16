5afc32ecb0502255869ff0a42c68a74c0cc363d7
esti08-hu <estioame@gmail.com>
2023-06-16 12:20:56 +0300

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -22,3 +23,4 @@ printf("%d is zero\n", n);
 }
 return (0);
 }
+
