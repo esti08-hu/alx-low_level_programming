b2d069160c04869df9585ecbe9808cc67872227d
esti08-hu <estioame@gmail.com>
2023-06-16 19:24:54 +0300

    test

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+int c = 0;
+
+while (c <= 10)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
+
