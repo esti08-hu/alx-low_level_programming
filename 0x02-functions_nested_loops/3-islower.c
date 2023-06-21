c45a820b29dc3f37f01652460292bf0da2de2dc6
esti08-hu <estioame@gmail.com>
2023-06-21 21:15:35 +0300

    test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,10 +7,9 @@
 */
 int _islower(int c) {
     
-    if (c >= 'a' && c <= 'z') {
-        return 1;  // lowercase character
-    } else {
-        return 0;  // not a lowercase character
-    }
+if (c >= 'a' && c <= 'z') {
+return 1;
+} else {
+return 0;
 }
-
+}
