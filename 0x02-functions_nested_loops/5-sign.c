ec451de01183480f9c1ed93f90dc0f6325736a37
esti08-hu <estioame@gmail.com>
2023-06-21 22:14:42 +0300

    test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,18 @@
 */
 int print_sign(int n) 
 {
-if (n > 0) {
-printf("+");
+if (n > 0) 
+{
+_putchar("+");
 return (1);
-} else if (n == 0) {
-printf("0");
+} 
+else if (n == 0) {
+_putchar("0");
 return (0);
-} else {
-printf("-"); 
+} 
+else 
+{
+_putchar("-"); 
 return (-1);
     }
 }
