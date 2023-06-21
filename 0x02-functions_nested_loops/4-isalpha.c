eb4a39ba6cdeeb4d51231ced41850cecd51e6b0d
esti08-hu <estioame@gmail.com>
2023-06-21 21:36:49 +0300

    test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
 * @c: the character to check
 * Return: 1 if c is lowercase, 0 otherwise
 */
-int int _isalpha(int c) {
+int _isalpha(int c) {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return(1);
 else
