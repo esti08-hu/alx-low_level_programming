08bfa143832b427901ecfc62470e818be43704fc
esti08-hu <estioame@gmail.com>
2023-06-26 20:06:56 +0300

    test

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-#include "head.h"
+#include "main.h"
 /*
  * This function takes two integer pointers as parameters and swaps the values of the integers
  * they point to. This allows you to exchange the values of two variables without using a
