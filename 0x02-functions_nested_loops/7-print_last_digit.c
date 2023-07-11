4fdefe14f29228897f1776f501739c56cf7fd4df
esti08-hu <estioame@gmail.com>
2023-07-11 12:04:11 +0300

    test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,15 @@
 
 int print_last_digit(int n)
 {
-    int lDigit;
+int lDigit;
 
-    if (n < 0)
-        n = -n;
+if (n < 0)
+n = -n;
+lDigit = n % 10;
+if (lDigit < 0)
+lDigit = -lDigit;
 
-    lDigit = n % 10;
+_putchar(lDigit + '0');
 
-    if (lDigit < 0)
-        lDigit = -lDigit;
-
-    _putchar(lDigit + '0');
-
-    return (lDigit);
+return (lDigit);
 }
