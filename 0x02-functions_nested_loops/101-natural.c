581c2a6f030e3c2d297870af09140c13879aabb3
esti08-hu <estioame@gmail.com>
2023-07-11 12:42:07 +0300

    tets

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
 int multiples()
 {
-    int i, j;
+    int i;
     int sum = 0;
     for (i = 1; i <= 1024; i++)
     {
