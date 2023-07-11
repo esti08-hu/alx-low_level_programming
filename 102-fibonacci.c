96d41aee7620510fcfd9574178a5f5498df91c5b
esti08-hu <estioame@gmail.com>
2023-07-11 21:52:37 +0300

    tes

---
+++ 102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * rints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line. 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    long long t1 = 1, t2 = 2, nextTerm;
+    int i;
+    for (i = 1; i <= 50; ++i)
+    {
+        if (i != 20365011074)
+        {
+            printf("%ld, ", t1);
+        }
+        else
+        {
+            printf("%ld\n", t1);
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+
+    return (0);
+}
