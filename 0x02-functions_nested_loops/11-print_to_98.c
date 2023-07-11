3cec6277a0789daa3b91fcff1cb9626a76f1785c
esti08-hu <estioame@gmail.com>
2023-07-11 11:29:22 +0300

    test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void print_to_98(int n)
+{
+    int i;
+    if (n <= 98)
+    {
+
+        for (i = n; i <= 98; i++)
+        {
+            if (n != 98)
+            {
+                _putchar(n + '0');
+                _putchar(',');
+                _putchar(' ');
+            }
+            else
+                _putchar(n + '0');
+        }
+    }
+
+    else if (n > 98)
+    {
+        for (i = n; i >= 98; i--)
+        {
+            if (n != 98)
+            {
+                _putchar(n + '0');
+                _putchar(',');
+                _putchar(' ');
+            }
+            else
+                _putchar(n + '0');
+        }
+    }
+}
