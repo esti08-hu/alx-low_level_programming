e4ef79926483fe7efcb4dc33fc295a1f2ecd87c5
esti08-hu <estioame@gmail.com>
2023-07-11 22:44:46 +0300

    test

--- 104-fibonacci.c
+++ 104-fibonacci.c
@@ -6,26 +6,38 @@
  * followed by a new line
  * Return: ALways 0 (Success)
  */
-
-int main()
+int main(void)
 {
- 
-    int first, second, numTerms;
-    first = 1;
-    second = 2;
-    numTerms = 98;
-    printf("%d, %d", first, second);
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-    for (int i = 3; i <= numTerms; ++i)
-    {
-        int next = first + second;
-        printf(", %d", next);
+	j = 1;
+	k = 2;
 
-        first = second;
-        second = next;
-    }
+	printf("%lu", j);
 
-    printf("\n");
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
 
-    return (0);
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
+	return (0);
 }
