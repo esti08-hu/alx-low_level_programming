68f142960a976a145cc0796fe0a48e6861676bdb
esti08-hu <estioame@gmail.com>
2023-07-11 12:46:48 +0300

    test

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,16 @@
  */
 int main()
 {
-    int i;
-    int sum = 0;
-    for (i = 1; i <= 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-    printf("%d", sum);
-    printf("\n");
-    return (0);
+int i;
+int sum = 0;
+for (i = 1; i <= 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d", sum);
+printf("\n");
+return (0);
 }
