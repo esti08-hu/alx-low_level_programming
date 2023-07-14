88d6a9c4f0729a37bbc75cf6263a2d4206e062cb
esti08-hu <estioame@gmail.com>
2023-07-14 22:20:38 +0300

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,6 +19,13 @@ s[i] = b;
 return (s);
 }
 
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr;
