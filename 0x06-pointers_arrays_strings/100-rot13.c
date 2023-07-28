0776b402a134a0ac013fac5715c543c3df5762ad
esti08-hu <estioame@gmail.com>
2023-07-29 02:31:27 +0300

    test

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,17 +6,12 @@
  *
  * Return: the resulting string
  */
-char *rot13(char *str)
-{
+char *rot13(char *str) {
     char *p = str;
-    while (*p != '\0')
-    {
-        if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
-        {
+    while (*p != '\0') {
+        if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M')) {
             *p += 13;
-        } 
-        else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
-        {
+        } else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z')) {
             *p -= 13;
         }
         p++;
