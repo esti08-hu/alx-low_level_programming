53779cb7db972dbef39fcab10d024e1b33c65754
esti08-hu <estioame@gmail.com>
2023-07-29 03:27:40 +0300

    test

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *argstostr - concatenates all arguments of the program.
-  *@ac: argument count.
-  *@av: pointer to array of size ac.
-  *Return: NULL if ac == 0 or av == null, Pointer to new string.
-  *NULL on fail.
-  */
-char *argstostr(int ac, char **av)
+ *argstostr - concatenates all arguments of the program.
+ *@ac: argument count.
+ *@av: pointer to array of size ac.
+ *Return: NULL if ac == 0 or av == null, Pointer to new string.
+ *NULL on fail.
+ */
+char *argstostr(int ac, const char **av)
 {
-	int i, j, k, size;
-	char *arg;
+    size_t i, j, k, size;
+    char *arg;
 
-	size = 0;
-	k = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			size++;
-			j++;
-		}
-		size++;
-		i++;
-	}
-	arg = malloc((sizeof(char) * size) + 1);
-	if (arg == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			arg[k] = av[i][j];
-			j++;
-			k++;
-		}
-		arg[k] = '\n';
-		k++;
-		i++;
-	}
-	arg[k] = '\0';
-	return (arg);
+    size = 0;
+
+    for (i = 0; i < (size_t)ac; i++)
+    {
+        size += strlen(av[i]) + 1; /* Add length of string plus newline */
+    }
+
+    arg = calloc(size, sizeof(*arg));
+    if (arg == NULL)
+    {
+        return (NULL);
+    }
+
+    k = 0;
+    for (i = 0; i < (size_t)ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+        {
+            arg[k++] = av[i][j];
+        }
+        arg[k++] = '\n';
+    }
+
+    arg[k] = '\0';
+
+    return (arg);
 }
