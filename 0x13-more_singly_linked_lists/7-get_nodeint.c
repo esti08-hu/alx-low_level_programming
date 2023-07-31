f70cad520988cc9868d87f508a4890390858426a
esti08-hu <estioame@gmail.com>
2023-08-01 00:11:18 +0300

    test

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,11 +15,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (count == index)
 		{
-			return head;
+			return (head);
 		}
 		head = head->next;
 		count++;
 	}
 
-	return(NULL);
+	return (NULL);
 }
