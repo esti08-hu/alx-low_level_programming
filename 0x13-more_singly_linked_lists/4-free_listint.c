cdb11c4e684b38ffd4f523a26c0ca10bcf194f73
esti08-hu <estioame@gmail.com>
2023-07-31 22:56:02 +0300

    test

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,8 +11,8 @@ void free_listint(listint_t *head)
     
 	while (head != NULL)
 	{
-		current = head;
-		head = head->next;
-		free(current);
+		current = head->next;
+		free(head);
+		head = current;
 	}
 }
