e3664703fa2b9a1652ce2a189d70842267fab71e
esti08-hu <estioame@gmail.com>
2023-08-01 00:52:28 +0300

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	if (current == NULL && count < idx)
+	if (count < idx || (current == NULL && count == idx))
 	{
 		free(new_node);
 		return (NULL);
