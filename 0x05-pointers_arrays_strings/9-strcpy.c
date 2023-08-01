6d6fde6bb93de133f30fe5b9b55add7d67ca34f6
esti08-hu <estioame@gmail.com>
2023-08-01 23:59:31 +0300

    test

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
@@ -11,16 +11,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i, size;
-    size = 0;
-    while (src[size] != '0')
-    {
-        size++;
-    }
-    for (i = 0; i < size; i++)
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-    return (dest);
+
+	return (dest);
 }
