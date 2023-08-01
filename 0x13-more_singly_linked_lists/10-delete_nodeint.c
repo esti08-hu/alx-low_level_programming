5dc0858f690f37bf914d0ad142fe1baa0c7e8a9b
esti08-hu <estioame@gmail.com>
2023-08-01 11:41:56 +0300

    test

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		count++;
 	}
 
-	if (current == NULL || current->next == NULL)
+	if (current == NULL || current->next == NULL || count != index - 1)
 	{
 		return (-1);
 	}
