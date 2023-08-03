9134e9b91709bb1b37868fd85effb72dd2a2ca75
esti08-hu <estioame@gmail.com>
2023-08-03 21:30:13 +0300

    test

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,20 @@ void print_binary(unsigned long int n)
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	int started = 0;
 
-	while (mask > 0) {
-		if ((n & mask) == mask) {
+	while (mask > 0)
+	{
+		if ((n & mask) == mask)
+		{
 			putchar('1');
 			started = 1;
-		} else if (started) {
+		} else if (started)
+		{
 			putchar('0');
 		}
 		mask >>= 1;
 	}
-	if (!started) {
+	if (!started)
+	{
 		putchar('0');
 	}
 }
