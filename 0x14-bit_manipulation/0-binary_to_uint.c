0198ac56f731fa538b08b5813b9a036b95d319e4
esti08-hu <estioame@gmail.com>
2023-08-03 15:07:35 +0300

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,9 +19,8 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	num = 1;
-	sum = 0;
-	for (i--; i >= 0; i--)
+
+	for (num = 1, sum = 0, i--; i >= 0; i--)
 	{
 		if (b[i] == '1')
 			sum += num;
