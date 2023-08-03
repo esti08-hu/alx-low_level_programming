a53f8c8dd349e3acbcb15bf2e5b8c109342ab8c7
esti08-hu <estioame@gmail.com>
2023-08-03 21:51:08 +0300

    clear the bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * @index: index position to change, starting from 0
  * Return: 1 if it worked, -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 	{
