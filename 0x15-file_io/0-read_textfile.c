b5d4eed93d1cf3e7eb64a3594f70590aff73c784
esti08-hu <98952769+esti08-hu@users.noreply.github.com>
2023-09-15 17:05:15 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,36 +3,37 @@
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filenamePtr: A pointer to the name of the file.
- * @numLetters: The number of letters the function should read and print.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: If the function fails or filenamePtr is NULL - 0.
+ * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filenamePtr, size_t numLetters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fileDescriptor, bytesRead, bytesWritten;
+	ssize_t o, r, w;
 	char *buffer;
 
-	if (filenamePtr == NULL)
+	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * numLetters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	fileDescriptor = open(filenamePtr, O_RDONLY);
-	bytesRead = read(fileDescriptor, buffer, numLetters);
-	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (fileDescriptor == -1 || bytesRead == -1 || bytesWritten == -1 || bytesWritten != bytesRead)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(fileDescriptor);
+	close(o);
 
-	return (bytesWritten);
+	return (w);
 }
